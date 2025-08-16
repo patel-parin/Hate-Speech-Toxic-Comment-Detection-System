453039c4449cc6a5493359088fd2022963c2860b
Patel Parin Rajesh <152582463+patel-parin@users.noreply.github.com>
2025-08-16 14:14:48 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,6 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cookieParser()); 
 
-const cors = require("cors");
 
 const allowedOrigins = [
   "https://hate-speech-toxic-comment-detection-one.vercel.app",
