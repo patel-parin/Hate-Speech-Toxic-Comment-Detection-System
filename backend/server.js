75a6e5fec14c743d46db526ab6186af6f6bd5d8b
Patel Parin Rajesh <152582463+patel-parin@users.noreply.github.com>
2025-08-16 11:05:40 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,9 +15,13 @@ const PORT = process.env.PORT || 5000;
 app.use(cookieParser()); 
 
 app.use(cors({
-  origin: 'https://hate-speech-toxic-comment-detection-one.vercel.app/'||'http://localhost:5173', // your Vite frontend
-  credentials: true
+  origin: [
+    "https://hate-speech-toxic-comment-detection-one.vercel.app",
+    "http://localhost:5173"
+  ],
+  credentials: true,
 }));
+
 app.use(express.json());
 
 
